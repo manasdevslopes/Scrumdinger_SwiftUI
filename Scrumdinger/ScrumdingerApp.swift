20ddddfcece1e30d34aea294076db8ef50649abe
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-09-10 13:07:13 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by MANAS VIJAYWARGIYA on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
